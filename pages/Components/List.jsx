11612669e21fd027a5ad68930c45b3a747797c2c
thearnabsaha <thearnabsaha201@gmail.com>
2022-01-24 18:08:47 +0530

    s1

--- pages/Components/List.jsx
+++ pages/Components/List.jsx
@@ -1,11 +1,14 @@
+import { MdOutlineDeleteOutline,MdOutlineDriveFileRenameOutline} from "react-icons/md";
 const List = () => {
     return (
         <>
             <div className="lists">
                 <div className="list">
-                <div className="task">
-                    <h1>Arnab Saha Is Best...</h1>
-                </div>
+                    <div className="task">
+                        <h1>Arnab Saha Is Best...</h1>
+                        <button><MdOutlineDriveFileRenameOutline/></button>
+                        <button><MdOutlineDeleteOutline/></button>
+                    </div>
                 </div>
             </div>
         </>
@@ -13,3 +16,4 @@ const List = () => {
 }
 
 export default List;
+
