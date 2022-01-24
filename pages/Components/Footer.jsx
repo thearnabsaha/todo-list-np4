ecec6cdc092c52b3a02e2bfbbd3f3dd3f7f45416
thearnabsaha <thearnabsaha201@gmail.com>
2022-01-24 18:15:04 +0530

    s1

---
+++ pages/Components/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    const year = new Date().getFullYear()
+    return (
+        <>
+            <h3>Copyright &copy; {year}</h3>
+        </>
+    );
+}
+
+export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Header from './Components/Header';
 import Input from './Components/Input';
 import List from './Components/List';
+import Footer from './Components/Footer';
 
 const index = () => {
   return (
@@ -12,6 +13,7 @@ const index = () => {
       <Header/>
       <Input/>
       <List/>
+      <Footer/>
     </>
   );
 }
