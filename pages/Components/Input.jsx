507632fc4e0f2835afad1b16aa7c3e1eb49c2d1d
thearnabsaha <thearnabsaha201@gmail.com>
2022-01-26 12:23:12 +0530

    s3

--- pages/Components/Input.jsx
+++ pages/Components/Input.jsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
 import { MdSend } from "react-icons/md";
 const Input = () => {
+    const [task, setTask] = useState("");
+    const [records, setRecords] = useState([]);
+    const handleChange=(e) => {
+        setTask(e.target.value)
+    }
+    const handleSubmit =(e) => {
+        e.preventDefault()
+        console.log(task);
+        setTask("")
+    }
     return (
         <>
             <div className="input">
-                <form action="">
-                    <input type="text" placeholder="Add Your Tasks..."/>
+                <form action="" onSubmit={handleSubmit}>
+                    <input type="text" placeholder="Add Your Tasks..." value={task} onChange={handleChange}/>
                     <button type="submit"><MdSend className="send"/></button>
                 </form>
             </div>
